7e639340ffff22faa2c0155be0af49f0a449d7c8
ivana0996 <ivanaescobar96@hotmail.com>
2025-09-11 23:03:12 -0300

    Ventana Marcas

---
+++ TrabajoPracticoDos/frmMarca.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace TrabajoPracticoDos
+{
+    partial class frmMarca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(190, 139);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(108, 49);
+            this.btnEliminar.TabIndex = 0;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMarca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEliminar);
+            this.Name = "frmMarca";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
---
+++ TrabajoPracticoDos/frmMarca.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dominio;
+using Negocio;
+
+namespace TrabajoPracticoDos
+{
+    public partial class frmMarca : Form
+    {
+        private List<Marca> listaMarcas;
+        private DataGridView dgvMarcas;
+
+        public frmMarca()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMarca_Load(object sender, EventArgs e)
+        {
+            cargar();
+
+        }
+
+        private void cargar() {
+
+            MarcaNegocio negocio = new MarcaNegocio();
+
+            try
+            {
+
+                listaMarcas = negocio.listar();
+                dgvMarcas.DataSource = listaMarcas;
+          
+                dgvMarcas.Columns["Id"].Visible = false;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }   
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Marca seleccionado = (Marca)dgvMarcas.CurrentRow.DataBoundItem;
+            MarcaNegocio negocio = new MarcaNegocio();
+            try
+            {
+            DialogResult respuesta = MessageBox.Show("¿Seguro que desea eliminar la marca " + seleccionado.Descripcion + "?", "Eliminando", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (respuesta==DialogResult.Yes)
+                {
+                    negocio.eliminarLogico(seleccionado.Id);
+                    cargar();
+                }
+               
+              }
+
+            catch (Exception ex)
+            {
+            MessageBox.Show(ex.ToString());
+
+            }
+        }
+    }
+}
